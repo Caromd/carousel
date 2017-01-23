0189bd3144d474f58cc5e32443e4de75c3789588
Caroline Deppe <rcdeppe@gmail.com>
2017-01-23 18:33:59 +0000

    Changing admin layouts to materialize

---
+++ app/assets/javascripts/nav.js
@@ -0,0 +1,15 @@
+// Initialize collapse button
+//('.button-collapse').sideNav({
+//    menuWidth: 100, // Default is 240
+//    edge: 'left', // Choose the horizontal origin
+//    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+//  }
+//);
+//(document).on('ready page:change', function() {
+// (".button-collapse").sideNav(); // Initialize collapseS
+//});
+//(function($){
+//  $(function(){
+//    $('.button-collapse').sideNav();
+//  }); // end of document ready
+//})(); // end of jQuery name space
